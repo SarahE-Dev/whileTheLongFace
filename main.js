8172b8b8913cbdc9250a43c21204a6736b9ea6fd
Sarah E <seatherly.prsvr@gmail.com>
2023-09-12 15:43:41 -0500

    finished

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 function numberDoubler(num) {
     // Your code here
+    while(num < 100){
+        num *= 2;
+    }
+    return num;
 }
 
 function stringRepeater(str) {
+    let result = '';
     // Your code here
+    while(result.length < 10){
+        result = str + result
+    }
+    return result;
 }
 
 function makeDivisible(x, y) {
+    while(x % y !== 0){
+        x += 1;
+    }
+    return x;
     // Your code here
 }
 
